e4c6d817058113ff1c83e4f0bb9a5e092d51493b
Nicholas igunbor <nickoklaus5@gmail.com>
2023-01-25 14:04:26 +0100

    metaverse madness

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,8 +1,34 @@
 'use client';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+import styles from '../styles';
+import { motion } from 'framer-motion';
+
 
 const Hero = () => (
-  <section>
-    Hero section
+  <section className={`${styles.yPaddings} sm-pl-16 pl-16`}>
+    <motion.div
+    variants={staggerContainer}
+    initial='hidden'
+    whileInView='show'
+    viewport={{once: false, amount: 0.25 }}
+    className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className='flex justify-center items-center flex-col relative z-10'>
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+          Metaverse
+        </motion.h1>
+        <motion.div
+          variants={textVariant(1.2)}
+          className='flex flex-grow justify-center items-center'
+          >
+            <h1 className={styles.heroHeading}>Ma</h1>
+            <div className={styles.heroDText}>D</div>
+            <h1 className={styles.heroHeading}>Ness</h1>
+        </motion.div>
+
+      </div>
+
+    </motion.div>
   </section>
 );
 
