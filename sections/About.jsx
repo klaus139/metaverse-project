6f06f8a7e6fac309e734c2c6616b9aa96e2d45c3
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-01-26 16:22:38 +0100

    ready for deployment

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,26 +1,28 @@
 'use client';
+
+import { motion } from 'framer-motion';
 import { fadeIn, staggerContainer } from '../utils/motion';
 import styles from '../styles';
-import { motion } from 'framer-motion';
-import { TypingText} from '../components';
-
+import { TypingText } from '../components';
 
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
-    <div className='gradient-02 z-0' />
+    <div className="gradient-02 z-0" />
     <motion.div
-    variants={staggerContainer}
-    initial='hidden'
-    whileInView='show'
-    viewport={{once: false, amount: 0.25}}
-    className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
       <TypingText title="| About Metaversus"
-      textStyles="text-center" />
+        textStyles="text-center"
+      />
       <motion.p
-      variants={fadeIn('up', 'tween', 0.2,1)}
-      className='mt-[8px] font-normal sm:text-[32px] text-[20px] tetx-center text-secondary-white'>
-         <span className="font-extrabold text-white">Metaverse</span> is a new
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] tetx-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Metaverse</span> is a new
         thing in the future, where you can enjoy the virtual world by feeling
         like it's really real, you can feel what you feel in this metaverse
         world, because this is really the{' '}
@@ -36,15 +38,13 @@ const About = () => (
 
       </motion.p>
       <motion.img
-      variants={fadeIn('up','tween', 0.3, 1)}
-      src='/arrow-down.svg'
-      alt='arrow down'
-      className='w-[180px] h-[28px] object-contain mt-[28px]'
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[180px] h-[28px] object-contain mt-[28px]"
       />
 
-     
     </motion.div>
-
 
   </section>
 );
